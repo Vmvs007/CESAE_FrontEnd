ceccfd0dfddfaa84e9c4227c35df902a78447422
Vitor Santos <vmvs007@gmail.com>
2023-06-19 13:44:50 +0100

    Ficha_03

---
+++ Ficha03/src/Ex_01.java
@@ -0,0 +1,9 @@
+public class Ex_01 {
+    public static void main(String[] args) {
+
+        // Declarar variáveis
+        int num1,num2;
+
+        System.out.println("***** Exercicio 1 *****");
+    }
+}

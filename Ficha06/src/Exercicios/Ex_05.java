707ccdd0918c5f81fdf97e052fd9268c2900e684
Vitor Santos <vmvs007@gmail.com>
2023-07-18 09:30:28 +0100

    Ficha_06 & Ficha_07

--- Ficha06/src/Exercicios/Ex_05.java
+++ Ficha06/src/Exercicios/Ex_05.java
@@ -1,4 +1,40 @@
 package Exercicios;
 
+import java.util.Scanner;
+
 public class Ex_05 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Introduza um caracter: ");
+        String caracter = input.next();
+
+        System.out.print("Introduza o número de linhas: ");
+        int linhas = input.nextInt();
+
+        System.out.print("Introduza o número de colunas: ");
+        int colunas = input.nextInt();
+
+        desenharQuadrado(caracter, linhas, colunas);
+    }
+
+    /**
+     * Procedimento que imprime um quadrado na consola, de acordo com o caracter e as medidas passadas pelo utilizador
+     * @param caracter Caracter através do qual desenha o quadrado
+     * @param linhas - Nº linhas do quadrado
+     * @param colunas - Nº colunas do quadrado
+     */
+    public static void desenharQuadrado(String caracter, int linhas, int colunas) {
+        System.out.println("");
+        for (int i = 0; i < linhas; i++) {
+            for (int x = 0; x < colunas; x++) {
+                if (i == 0 || i == linhas - 1 || x == 0 || x == colunas - 1) {
+                    System.out.print(caracter);
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
---
+++ Ficha07/src/Ex_01.java
@@ -0,0 +1,13 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Ex_01 {
+    public static void main(String[] args) throws FileNotFoundException {
+
+    }
+}
+
+
+
+
